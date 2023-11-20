cb0c381b3a7f56f6ee348f44fc5dd1666e9dc191
arielLevita <levita.ariel@gmail.com>
2023-11-20 12:34:56 -0300

    Menu page.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,164 +3,164 @@ import FooterLinks from "../FooterLinks/FooterLinks"
 const Footer = () => {
 
     const footerLinks = {
-            about: [
-                {
-                    title: 'Nuestra historia',
-                    to:''
-                },
-                {
-                    title: 'Nuestra misión',
-                    to:''
-                },
-                {
-                    title: 'Nuestra cultura inclusiva y valores',
-                    to:''
-                },
-                {
-                    title: 'Nuestro café',
-                    to:''
-                },
-                {
-                    title: 'Historias y novedades',
-                    to:''
-                },
-                {
-                    title: 'Trabaja con nosotros',
-                    to:''
-                },
-                {
-                    title: 'Comunicados oficiales',
-                    to:''
-                },
-            ],
-            social: [
-                {
-                    title: 'Planeta',
-                    to: ''
-                },
-                {
-                    title: 'Personas',
-                    to: ''
-                },
-            ],
-            client: [
-                {
-                    title: 'Contacto',
-                    to: ''
-                },
-                {
-                    title: 'Medios de pago',
-                    to: ''
-                },
-                {
-                    title: 'Defensa de las y los consumidores',
-                    to: ''
-                },
-            ],
-            experience: [
-                {
-                    title: 'Formas de comprar',
-                    to: ''
-                },
-                {
-                    title: 'Delivery',
-                    to: ''
-                }
-            ],
-            general: [
-                {
-                    title: 'Accesibilidad web',
-                    to: ''
-                },
-                {
-                    title: 'Aviso de privacidad',
-                    to: ''
-                },
-                {
-                    title: 'Condiciones de uso',
-                    to: ''
-                },
-                {
-                    title: 'Mapa del sitio',
-                    to: ''
-                },
-            ]
-        }
+        about: [
+            {
+                title: 'Nuestra historia',
+                to: ''
+            },
+            {
+                title: 'Nuestra misión',
+                to: ''
+            },
+            {
+                title: 'Nuestra cultura inclusiva y valores',
+                to: ''
+            },
+            {
+                title: 'Nuestro café',
+                to: ''
+            },
+            {
+                title: 'Historias y novedades',
+                to: ''
+            },
+            {
+                title: 'Trabaja con nosotros',
+                to: ''
+            },
+            {
+                title: 'Comunicados oficiales',
+                to: ''
+            },
+        ],
+        social: [
+            {
+                title: 'Planeta',
+                to: ''
+            },
+            {
+                title: 'Personas',
+                to: ''
+            },
+        ],
+        client: [
+            {
+                title: 'Contacto',
+                to: ''
+            },
+            {
+                title: 'Medios de pago',
+                to: ''
+            },
+            {
+                title: 'Defensa de las y los consumidores',
+                to: ''
+            },
+        ],
+        experience: [
+            {
+                title: 'Formas de comprar',
+                to: ''
+            },
+            {
+                title: 'Delivery',
+                to: ''
+            }
+        ],
+        general: [
+            {
+                title: 'Accesibilidad web',
+                to: ''
+            },
+            {
+                title: 'Aviso de privacidad',
+                to: ''
+            },
+            {
+                title: 'Condiciones de uso',
+                to: ''
+            },
+            {
+                title: 'Mapa del sitio',
+                to: ''
+            },
+        ]
+    }
 
 
     return (
-        <footer>
-        <div className="wrapper">
-            <div className="flex flex-col lg:flex-row my-4">
-                <div className="lg:flex lg:justify-between leading-5">
-                    <div className="mx-4">
-                        <h6 className="my-4">Sobre nosotros</h6>
-                        <ul className="list-none space-y-4">
-                            {
-                                footerLinks.about?.map((link) => (
-                                    <FooterLinks key={link.title} title={link.title} to={link.to} />
-                                ))
-                            }
-                        </ul>
+        <footer className="border border-t-gray-300 mt-8">
+            <div className="wrapper">
+                <div className="flex flex-col lg:flex-row my-4">
+                    <div className="lg:flex lg:justify-between leading-5">
+                        <div className="mx-4">
+                            <h6 className="my-4">Sobre nosotros</h6>
+                            <ul className="list-none space-y-4">
+                                {
+                                    footerLinks.about?.map((link) => (
+                                        <FooterLinks key={link.title} title={link.title} to={link.to} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+
+                        <div className="mx-4">
+                            <h6 className="my-4">Impacto social</h6>
+                            <ul className="list-none space-y-4">
+                                {
+                                    footerLinks.social?.map((link) => (
+                                        <FooterLinks key={link.title} title={link.title} to={link.to} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+
+                        <div className="mx-4">
+                            <h6 className="my-4">Atención al cliente</h6>
+                            <ul className="list-none space-y-4">
+                                {
+                                    footerLinks.client?.map((link) => (
+                                        <FooterLinks key={link.title} title={link.title} to={link.to} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+
+                        <div className="mx-4">
+                            <h6 className="my-4">Experiencia Starbucks</h6>
+                            <ul className="list-none space-y-4">
+                                {
+                                    footerLinks.experience?.map((link) => (
+                                        <FooterLinks key={link.title} title={link.title} to={link.to} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
                     </div>
 
-                    <div className="mx-4">
-                        <h6 className="my-4">Impacto social</h6>
-                        <ul className="list-none space-y-4">
-                            {
-                                footerLinks.social?.map((link) => (
-                                    <FooterLinks key={link.title} title={link.title} to={link.to} />
-                                ))
-                            }
-                        </ul>
-                    </div>
-
-                    <div className="mx-4">
-                        <h6 className="my-4">Atención al cliente</h6>
-                        <ul className="list-none space-y-4">
-                            {
-                                footerLinks.client?.map((link) => (
-                                    <FooterLinks key={link.title} title={link.title} to={link.to} />
-                                ))
-                            }
-                        </ul>
-                    </div>
-
-                    <div className="mx-4">
-                        <h6 className="my-4">Experiencia Starbucks</h6>
-                        <ul className="list-none space-y-4">
-                            {
-                                footerLinks.experience?.map((link) => (
-                                    <FooterLinks key={link.title} title={link.title} to={link.to} />
-                                ))
-                            }
-                        </ul>
+                    <div className="px-3 lg:ml-auto my-4">
+                        <img className="h-20" src="https://i.postimg.cc/wMxZWG52/footer-argentina.jpg" alt="" />
                     </div>
                 </div>
 
-                <div className="px-3 lg:ml-auto my-4">
-                    <img className="h-20" src="https://i.postimg.cc/wMxZWG52/footer-argentina.jpg" alt="" />
+                <div className="block sm:flex flex-col items-center shadow shadow-slate-200 mt-4 px-4">
+                    <div className="my-4">
+                        <i className="text-3xl bi bi-facebook mx-2"></i>
+                        <i className="text-3xl bi bi-instagram mx-2"></i>
+                        <i className="text-3xl bi bi-linkedin mx-2"></i>
+                    </div>
+                    <ul className="list-none my-2 max-w-1/2 sm:flex justify-between sm:space-x-6 mb-4">
+                        {
+                            footerLinks.general?.map((link) => (
+                                <FooterLinks key={link.title} title={link.title} to={link.to} />
+                            ))
+                        }
+                    </ul>
+                    <h6 className="text-gray-400 no-underline mt-3 mb-0">&#169 2023. Starbucks Coffee Company. Reservados todos los derechos</h6>
+                    <small className="text-gray-400 no-underline break-words break-all pb-3">ar | prod | 44e96568eeacba34abb9b4f72e538133b5877b273 | 01/09/2023-05:08:44:762</small>
                 </div>
             </div>
-
-            <div className="block sm:flex flex-col items-center shadow shadow-slate-200 mt-4 px-4">
-                <div className="my-4">
-                    <i className="text-3xl bi bi-facebook mx-2"></i>
-                    <i className="text-3xl bi bi-instagram mx-2"></i>
-                    <i className="text-3xl bi bi-linkedin mx-2"></i>
-                </div>
-                <ul className="list-none my-2 max-w-1/2 sm:flex justify-between sm:space-x-6 mb-4">
-                    {
-                        footerLinks.general?.map((link) => (
-                            <FooterLinks key={link.title} title={link.title} to={link.to} />
-                        ))
-                    }
-                </ul>
-                <h6 className="text-gray-400 no-underline mt-3 mb-0">&#169 2023. Starbucks Coffee Company. Reservados todos los derechos</h6>
-                <small className="text-gray-400 no-underline break-words break-all pb-3">ar | prod | 44e96568eeacba34abb9b4f72e538133b5877b273 | 01/09/2023-05:08:44:762</small>
-            </div>
-        </div>
-    </footer>
+        </footer>
     )
 }
 
--- src/routes/Menu/Menu.jsx
+++ src/routes/Menu/Menu.jsx
@@ -63,18 +63,42 @@ const Menu = () => {
 
     return (
         <div>
-        {/* <div className="inline-block rounded-full hover:ring-4 hover:ring-inset ring-blue-900 m-8 p-8">
-            Menú de cafés
-        </div> */}
-
-            <div className="flex flex-wrap mx-auto">
-                {
-                    menu.bebidas.map((bebida) => (
-                        <MenuItem key={bebida.title} title={bebida.title} img={bebida.img} />
-                    ))
-                }
+            <div className="wrapper px-4">
+                <div className="py-8">
+                    <p className="font-medium">¡Disfrutalos!</p>
+                    <h2 className="font-bold">Conoce nuestras bebidas y alimentos de temporada</h2>
+                </div>
+                <div className="py-4 border-y border-black font-medium">
+                    <p>Bebidas</p>
+                </div>
+                <div className="flex flex-wrap mx-auto my-8">
+                    {
+                        menu.bebidas.map((bebida) => (
+                            <MenuItem key={bebida.title} title={bebida.title} img={bebida.img} />
+                        ))
+                    }
+                </div>
+                <div className="py-4 border-y border-black font-medium">
+                    <p>Comidas</p>
+                </div>
+                <div className="flex flex-wrap mx-auto my-8">
+                    {
+                        menu.comidas.map((comida) => (
+                            <MenuItem key={comida.title} title={comida.title} img={comida.img} />
+                        ))
+                    }
+                </div>
+                <div className="py-4 border-y border-black font-medium">
+                    <p>Bebidas</p>
+                </div>
+                <div className="flex flex-wrap mx-auto my-8">
+                    {
+                        menu.enCasa.map((item) => (
+                            <MenuItem key={item.title} title={item.title} img={item.img} />
+                        ))
+                    }
+                </div>
             </div>
-
         </div>
     )
 }
